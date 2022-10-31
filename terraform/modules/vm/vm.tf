c6c431e2ce63bed92c784acfa9c192e6e553ce2d
Việt Anh Hoàng <anhhv09021993@gmail.com>
2022-10-31 12:50:53 +0700

    Add publickey

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -28,7 +28,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   ]
   admin_ssh_key {
     username   = "anhhv3"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("./id_rsa.pub")
   }
   os_disk {
     caching              = "ReadWrite"
