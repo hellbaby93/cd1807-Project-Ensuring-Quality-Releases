269072a0e5550f19f36857e25ec5a828b7eb1680
Việt Anh Hoàng <anhhv09021993@gmail.com>
2022-11-02 00:47:51 +0700

    Add Selenium Tests

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -10,18 +10,66 @@ def config_driver():
     options.add_argument('--headless')
     return webdriver.Chrome(options=options)
 
-# Start the browser and login with standard_user
-def test_login (username, password):
-    driver = config_driver()
-    driver.get('https://www.saucedemo.com/')
 
-    driver.find_element(by=By.ID, value='user-name').send_keys(username)
-    driver.find_element(by=By.ID, value='password').send_keys(password)
-    driver.find_element(by=By.ID, value='login-button').click()
+def test_login(username='standard_user', password='secret_sauce'):
+    # Start the browser and login with standard_user
+    print("Testing Logging In")
+    DRIVER.get('https://www.saucedemo.com/')
 
-    print(driver.current_url)
+    DRIVER.find_element(by=By.ID, value='user-name').send_keys(username)
+    DRIVER.find_element(by=By.ID, value='password').send_keys(password)
+    DRIVER.find_element(by=By.ID, value='login-button').click()
 
-    assert 'https://www.saucedemo.com/inventory.html' == driver.current_url
+    assert 'https://www.saucedemo.com/inventory.html' == DRIVER.current_url
+    print("Testing Logging In Passed")
 
-test_login('standard_user', 'secret_sauce')
 
+def test_add_to_cart_button(username='standard_user', password='secret_sauce'):
+    print("Testing Add to Cart Buttons")
+
+    DRIVER.get('https://www.saucedemo.com/inventory.html')
+    print("Getting add cart buttons")
+    add_to_cart_btns = [item for item in DRIVER.find_elements(
+        By.CSS_SELECTOR, "button") if item.accessible_name.lower() == "Add to cart".lower()]
+    remove_cart_btns = [item for item in DRIVER.find_elements(
+        By.CSS_SELECTOR, "button") if item.accessible_name.lower() == "Remove".lower()]
+    add_to_cart_btn_count_init = len(add_to_cart_btns)
+    print("Check if add to cart button count > 0 and Remove cart button count == 0")
+    assert add_to_cart_btn_count_init > 0 and len(remove_cart_btns) == 0
+    print("PASSED!")
+
+    print("Clicking add to cart buttons")
+    for item in add_to_cart_btns:
+        item.click()
+
+    add_to_cart_btns = [item for item in DRIVER.find_elements(
+        By.CSS_SELECTOR, "button") if item.accessible_name.lower() == "Add to cart".lower()]
+    remove_cart_btns = [item for item in DRIVER.find_elements(
+        By.CSS_SELECTOR, "button") if item.accessible_name.lower() == "Remove".lower()]
+
+    print("Check if add to cart button count == 0 and Remove cart button count == initial button count")
+    assert len(remove_cart_btns) == add_to_cart_btn_count_init and len(
+        add_to_cart_btns) == 0
+    print("PASSED!")
+
+    print("Clicking Remove buttons")
+    for item in remove_cart_btns:
+        item.click()
+
+    add_to_cart_btns = [item for item in DRIVER.find_elements(
+        By.CSS_SELECTOR, "button") if item.accessible_name.lower() == "Add to cart".lower()]
+    remove_cart_btns = [item for item in DRIVER.find_elements(
+        By.CSS_SELECTOR, "button") if item.accessible_name.lower() == "Remove".lower()]
+
+    print("Check if add to cart button count == initial button count and Remove cart button count == 0")
+
+    assert len(add_to_cart_btns) == add_to_cart_btn_count_init and len(
+        remove_cart_btns) == 0
+    print("PASSED!")
+
+
+
+
+DRIVER = config_driver()
+test_login()
+test_add_to_cart_button()
