1fc8d6ac75898bf61ea703451d1a9882de5bd44e
Việt Anh Hoàng <anhhv09021993@gmail.com>
2022-11-01 23:49:17 +0700

    Update Packer

--- packer/azure-devops.sh
+++ packer/azure-devops.sh
@@ -6,6 +6,10 @@ curl -fkSL -o vstsagent.tar.gz https://vstsagentpackage.azureedge.net/agent/2.21
 tar -zxvf vstsagent.tar.gz;
 if [ -x "$(command -v systemctl)" ];
 then
-    ./config.sh --environment --environmentname "udacity" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/hellbaby93/ --work _work --projectname 'hellbaby93' --auth PAT --token 3ua5l5lqzcqepzrxzp6bks7w72wk4dc2dlnuavoomoudy5r3bfha --runasservice; sudo ./svc.sh install; sudo ./svc.sh start; else ./config.sh --environment --environmentname "udacity" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/hellbaby93/ --work _work --projectname 'hellbaby93' --auth PAT --token 3ua5l5lqzcqepzrxzp6bks7w72wk4dc2dlnuavoomoudy5r3bfha;
+    ./config.sh --environment --environmentname "udacity" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/hellbaby93/ --work _work --projectname 'hellbaby93' --auth PAT --token $AZURE_DEVOPS_PAT --runasservice;
+    sudo ./svc.sh install;
+    sudo ./svc.sh start;
+else
+    ./config.sh --environment --environmentname "udacity" --acceptteeeula --agent $HOSTNAME --url https://dev.azure.com/hellbaby93/ --work _work --projectname 'hellbaby93' --auth PAT --token AZURE_DEVOPS_PAT;
     ./run.sh;
 fi
